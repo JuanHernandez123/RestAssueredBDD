f78261cdffa5cae3be7652f04c2c48965788802f
unknown <juanmhernandezr3@gmail.com>
2021-10-19 18:36:45 -0500

    Get example using BDD

---
+++ src/test/java/APITestCases/TC001_GET_Request.java
@@ -0,0 +1,16 @@
+package APITestCases;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class TC001_GET_Request {
+
+	@Test
+	void getAnUser() {
+		given().when().get("https://reqres.in/api/users/2").then().statusCode(200)
+				.assertThat().body("data.email", equalTo("janet.weaver@reqres.in"))
+				.header("Content-Type", "application/json; charset=utf-8");
+	}
+
+}
